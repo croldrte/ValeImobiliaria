cb5773c2e38cd0731b5cba2080e104beacb49acf
Carol Duarte <carolduarte200@gmail.com>
2024-11-28 11:10:21 -0300

    Inicia desenvolvimento do recurso de pesquisa de imóveis

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ function changeNavbarBackground() {
     navbar.classList.remove("scrolled");
   }
 }
+
+// Função para redirecionar para a página de busca
+function buscarImoveis() {
+  const tipoPesquisa = document.getElementById('tipoPesquisa').value;
+  const searchTerm = document.getElementById('searchInput').value;
+
+  // Cria a URL para redirecionar à página de busca com parâmetros de pesquisa
+  const url = `busca.html?tipo=${tipoPesquisa}&termo=${encodeURIComponent(searchTerm)}`;
+  
+  // Redireciona para a página de busca
+  window.location.href = url;
+}
+
+// Adiciona evento de busca ao botão
+document.getElementById('searchButton').addEventListener('click', buscarImoveis);
+
+// Adiciona evento de pesquisa ao pressionar Enter no campo de pesquisa
+document.getElementById('searchInput').addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    buscarImoveis();
+  }
+});
