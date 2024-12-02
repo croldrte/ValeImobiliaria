ff3931440459ffa255d03b567d052e615e983434
Carol Duarte <carolduarte2000@gmail.com>
2024-12-02 11:14:35 -0300

    Implementa busca avançada

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
 };
 
 function changeNavbarBackground() {
-  var navbar = document.getElementById("navbar");
+  var navbar = document.getElementById("navbar-main");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navbar.classList.add("scrolled");
   } else {
@@ -19,7 +19,7 @@ function buscarImoveis() {
 
   // Cria a URL para redirecionar à página de busca com parâmetros de pesquisa
   const url = `busca.html?tipo=${tipoPesquisa}&termo=${encodeURIComponent(searchTerm)}`;
-  
+
   // Redireciona para a página de busca
   window.location.href = url;
 }
@@ -28,8 +28,20 @@ function buscarImoveis() {
 document.getElementById('searchButton').addEventListener('click', buscarImoveis);
 
 // Adiciona evento de pesquisa ao pressionar Enter no campo de pesquisa
-document.getElementById('searchInput').addEventListener('keypress', function(event) {
+document.getElementById('searchInput').addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
     buscarImoveis();
   }
 });
+
+// Ocultar/mostrar filtros avançados
+document.getElementById('toggleFilters').addEventListener('click', function () {
+  var filters = document.getElementById('advancedFilters');
+  filters.classList.toggle('d-none');
+});
+
+if (advancedFilters.classList.contains('d-none')) {
+  this.textContent = '+ Filtros';
+} else {
+  this.textContent = '- Filtros';
+}
